e84dc076a7fd330b18d457a50e36ff46a3a3c162
Stid <stid.rodriguez@ceiba.com.co>
2019-01-09 13:54:24 -0500

    Se agrega el proyecto

---
+++ src/test/java/com/ceiba/estacionamiento/stid/rodriguez/HolaMundoTest.java
@@ -0,0 +1,14 @@
+package com.ceiba.estacionamiento.stid.rodriguez;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HolaMundoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);;
+	}
+
+}
