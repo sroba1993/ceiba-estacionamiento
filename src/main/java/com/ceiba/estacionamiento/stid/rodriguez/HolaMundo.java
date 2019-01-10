e99ac5d5f80103857751f11537b81cb8e96d85bf
Stid <stid.rodriguez@ceiba.com.co>
2019-01-10 11:30:02 -0500

    Ultima modificacion para probar integracion continua

--- src/main/java/com/ceiba/estacionamiento/stid/rodriguez/HolaMundo.java
+++ src/main/java/com/ceiba/estacionamiento/stid/rodriguez/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hello world");
+		System.out.println("Hey");
 
 	}
-
 }
